ddf49fa2d788dea5056069c622fcc725a426895a
lyla-e <roseevans498@gmail.com>
2026-09-03 21:58:08 -0400

    added javascript and changed api keys

---
+++ src/main.js
@@ -0,0 +1,11 @@
+const API_KEY = import.meta.env.VITE_NASA_API_KEY;
+
+document.querySelector("#app").innerHTML = "<p>loading...</p>";
+
+fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
+.then(response => response.json()).then(data => {
+    console.log(data);
+})
+.then(data => {
+    document.querySelector("#app").innerHTML = `${data.title}`;
+})
